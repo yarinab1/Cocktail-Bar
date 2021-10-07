50582b3f1084a97496aac30ae51edb6644fdd90a
yarinab1 <57402773+yarinab1@users.noreply.github.com>
2021-10-07 16:01:14 +0300

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div id="footer">
-            from Footer
+            from fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffFooter
         </div>
     )
 }
